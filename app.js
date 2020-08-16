55c6190762bfe6f9d8f861d47b10eb8f6b4216a7
lerv <lervingenecatolico@yahoo.com>
2020-08-16 12:21:36 +0800

    added filter feature

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const todoInput = document.querySelector(".todo-input");
 const todoBtn = document.querySelector(".todo-btn");
 const todoList = document.querySelector(".todo-list");
 
+const todoFilter = document.querySelector(".filter-todo");
+
 // Listeners
 todoBtn.addEventListener("click", addTodo);
+todoList.addEventListener("click", updateList);
+todoFilter.addEventListener("change", filterList);
 
 //Functions 
-
 function addTodo(event) {
     //Prevent from submitting
     event.preventDefault();
@@ -28,7 +31,7 @@ function addTodo(event) {
     checkBtn.innerHTML = "<i class='fas fa-check'></i>";
     checkBtn.classList.add("checkBtn");
     todoDiv.appendChild(checkBtn);
-    
+
     //add a delete button
     const deleteBtn = document.createElement("button");
     deleteBtn.innerHTML = "<i class='fas fa-trash-alt'></i>";
@@ -38,4 +41,66 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
 
     todoInput.value = "";
+
+    filterList(event);
+}
+
+function updateList(event) {
+
+    const item = event.target;
+
+    //delete function
+    if (item.classList[0] === "deleteBtn") {
+        const todo = item.parentElement;
+        todo.classList.add("slide-out");
+        todo.addEventListener("transitionend", function () {
+            todo.remove();
+        })
+        // todo.remove();
+    }
+
+    //taskcomplete function
+    if (item.classList[0] === "checkBtn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+
+}
+
+//filter function
+function filterList(event) {
+
+    const todoDivs = todoList.childNodes;
+
+    switch (todoFilter.value) {
+        case "All":
+            todoDivs.forEach(element => {
+                element.classList.remove("hidden");
+            });
+            break;
+        case "Completed":
+            todoDivs.forEach(element => {
+                if (element.classList.contains("completed")) {
+                    element.classList.remove("hidden");
+                } else {
+                    element.classList.add("hidden");
+                }
+            });
+            break;
+        case "Not Completed":
+            todoDivs.forEach(element => {
+                if (element.classList.contains("completed")) {
+                    element.classList.add("hidden");
+                } else {
+                    element.classList.remove("hidden");
+                }
+            });
+            break;
+        default:
+            //same as case: ALL
+            todoDivs.forEach(element => {
+                element.classList.remove("hidden");
+            });
+            break;
+    }
 }
